d7baf8223b0bf5d0420e33eb9e2e34cd9234a613
vengersmoon <vengersmon003@gmail.com>
2024-11-26 15:15:58 +0530

    Flutter Different widgets

--- lib/main.dart
+++ lib/main.dart
@@ -27,23 +27,20 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+        return Scaffold(
+             appBar: AppBar(
+             title: Text('Flutter Container'),
+             ),
+            body:  Container(
+            width: 100,
+            height: 100,
+            color: Colors.cyan,
+            child: Text('Hello Dev!'),
+      ),
+    );     
   }
 }
 
-  @override
-  Widget build(BuildContext context) {
-  
-    return Scaffold(
-      appBar: AppBar(
-       
-        title: Text('Flutter Container'),
-      ),
-      body:  Container(
-
-      ),
-
-    );
-  }
+ 
 
   
