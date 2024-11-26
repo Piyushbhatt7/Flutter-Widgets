31a69009a6c8b24f97ffd6e765301b2f314a40b4
vengersmoon <vengersmon003@gmail.com>
2024-11-26 13:53:22 +0530

    Flutter Different widgets

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-      
+        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -40,7 +41,9 @@ class MyHomePage extends StatelessWidget {
        
         title: Text('Flutter Container'),
       ),
-      body:  Container(),
+      body:  Container(
+        
+      ),
 
     );
   }
