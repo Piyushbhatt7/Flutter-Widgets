a4705eb41a25c3e12a6bfb1e7cd03b0d28728631
vengersmoon <vengersmon003@gmail.com>
2024-11-26 13:57:43 +0530

    Flutter Different widgets

--- lib/main.dart
+++ lib/main.dart
@@ -37,12 +37,10 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
        
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-       
         title: Text('Flutter Container'),
       ),
       body:  Container(
-        
+
       ),
 
     );
