9b4536f388a92626e91149b07408e7764dc17f2f
vengersmoon <vengersmon003@gmail.com>
2024-11-27 12:18:36 +0530

    Adding an image

--- lib/How to add image.dart
+++ lib/How to add image.dart
@@ -1,17 +1,19 @@
+                                 // to add an image you need to store your image in different "folder" or "directory" and must need to add in a pubspec.yaml
+
 import 'package:flutter/material.dart';
 
 class adding_image extends StatelessWidget {
   const adding_image({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) {                  
     return Scaffold(
       appBar: AppBar(
         title: Text('Adding an image'),
         backgroundColor: Colors.amberAccent,
       ),
        
-       
+
 
 
     );
