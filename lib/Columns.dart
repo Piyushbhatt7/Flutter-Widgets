70c919a77b7bf3c29d9bf083615246ac842c1250
vengersmoon <vengersmon003@gmail.com>
2024-11-28 11:10:34 +0530

    How columns work

---
+++ lib/Columns.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Columns extends StatelessWidget {
+  const Columns({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('How Row Works'),
+        backgroundColor: Colors.greenAccent,
+      ),
+    );
+  }
+}
