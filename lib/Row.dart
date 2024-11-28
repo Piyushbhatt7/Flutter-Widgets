a0f90374881bafaa8f378c02281bc930bdb9c3be
vengersmoon <vengersmon003@gmail.com>
2024-11-28 11:25:51 +0530

    How columns work

--- lib/Row.dart
+++ lib/Row.dart
@@ -4,7 +4,7 @@ class Rows extends StatelessWidget {
   const Rows({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) {               // row are horizontal  _______________
     return Scaffold(
       appBar: AppBar(
         title: Text('How Row Works'),
