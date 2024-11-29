51eb26f8d85f0a7ea3981818fa4b68b7883876c0
vengersmoon <vengersmon003@gmail.com>
2024-11-29 19:37:26 +0530

    Scroll view widget

--- lib/scrollview.dart
+++ lib/scrollview.dart
@@ -10,6 +10,41 @@ class scrollview extends StatelessWidget {
         title: Text('Scroll View'),
         backgroundColor: Colors.pinkAccent,
       ),
+
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(bottom: 10),
+            height: 200,
+            width: 200,
+            color: Colors.green,
+          ),
+          Container(
+            margin: EdgeInsets.only(bottom: 10),
+            height: 200,
+            width: 200,
+            color: Colors.pink,
+          ),
+          Container(
+            margin: EdgeInsets.only(bottom: 10),
+            height: 200,
+            width: 200,
+            color: Colors.black,
+          ),
+          Container(
+            margin: EdgeInsets.only(bottom: 10),
+            height: 200,
+            width: 200,
+            color: Colors.yellow,
+          ),
+          Container(
+            margin: EdgeInsets.only(bottom: 10),
+            height: 200,
+            width: 200,
+            color: Colors.blue,
+          ),
+        ],
+      ),
     );
   }
 }
