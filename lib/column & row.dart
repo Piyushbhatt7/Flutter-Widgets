bc55426eae539815b39121f55c9adcc63d167d1f
vengersmoon <vengersmon003@gmail.com>
2024-11-29 13:13:08 +0530

    Row and Column widgets

---
+++ lib/column & row.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class Columns extends StatelessWidget {
+  const Columns({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(                                       // Columns are vertical |
+                                                                               //   |
+        title: Text('How Column Works'),
+        backgroundColor: Colors.redAccent,
+      ),
+
+      body: Container(
+        height: 300,
+        width: 300,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Row(
+              children: [
+            Text('R1',style: TextStyle(fontSize: 25),),
+            Text('R2',style: TextStyle(fontSize: 25),),
+            Text('R3',style: TextStyle(fontSize: 25),),
+            Text('R4',style: TextStyle(fontSize: 25),),
+            Text('R5',style: TextStyle(fontSize: 25),),
+              ],
+            )
+            
+
+          //  ElevatedButton(onPressed: (){
+
+          //  }, child: Text('click'))
+          ],
+          
+        ),
+      ),
+    );
+  }
+}
