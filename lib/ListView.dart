664d5b5958b14ea76b121945945286117de1316f
vengersmoon <vengersmon003@gmail.com>
2024-11-29 22:58:39 +0530

    ListView widget

---
+++ lib/ListView.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Listview extends StatelessWidget {
+  const Listview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ListView Widget'),
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+    );
+  }
+}
