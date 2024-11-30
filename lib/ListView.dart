4440e64d050d66a2a113b33dc06409716228a969
vengersmoon <vengersmon003@gmail.com>
2024-11-30 10:11:14 +0530

    List View Widget

--- lib/ListView.dart
+++ lib/ListView.dart
@@ -13,6 +13,7 @@ class Listview extends StatelessWidget {
 
       body: ListView(
         scrollDirection: Axis.horizontal,
+        reverse: true,
         children: [
           Padding(
             padding: const EdgeInsets.all(8.0),
