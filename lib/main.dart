43281899b6c1aee9bef20ff8ff477ff8026a2711
vengersmoon <vengersmon003@gmail.com>
2024-11-30 14:14:46 +0530

    main dart file

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,9 @@ main(){
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      title: 'Flutter Application',
+    );
     
   }
   
