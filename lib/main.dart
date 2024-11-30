9d08c3c7eed4c92e243405393e69d159928875ce
vengersmoon <vengersmon003@gmail.com>
2024-11-30 14:07:48 +0530

    main dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_wigets/Columns.dart';
-import 'package:flutter_wigets/Inkwell.dart';
-import 'package:flutter_wigets/ListView.dart';
-import 'package:flutter_wigets/ListviewBuilder.dart';
-import 'package:flutter_wigets/Row.dart';
-import 'package:flutter_wigets/column%20&%20row.dart';
-import 'package:flutter_wigets/scrollview.dart';
 
-
-void main() {
-  runApp(const MyApp());
+main(){
+  runApp(MyApp);
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override 
-  Widget build(BuildContext context) {
-        return Scaffold(
-             appBar: AppBar(
-             title: Text('Flutter Container'),
-             ),
-            body:  Text("Flutter development", style: TextStyle(
-              color: Colors.pinkAccent,
-              fontSize: 23,
-              fontWeight: FontWeight.w900,
-              backgroundColor: Colors.black
-            ),),
-   
-    ); 
-  }
-}
-
- 
-
-  
