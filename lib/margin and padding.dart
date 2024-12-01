d8078d350cc3788c58dcdaf9f35544d80891352d
vengersmoon <vengersmon003@gmail.com>
2024-12-01 22:28:27 +0530

    Margin and Padding

---
+++ lib/margin and padding.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class margin_and_padding extends StatelessWidget {
+  const margin_and_padding({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Margin and Padding'),
+        
+      ),
+    );
+  }
+}
