af73f5995a26f1e95f3d3c9cba675cec6ad2ea6b
vengersmoon <vengersmon003@gmail.com>
2024-12-01 12:10:08 +0530

    AddingDecorationToContainer

---
+++ lib/adding_decoration_to_container.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddingDecorationToContainer extends StatelessWidget {
+  const AddingDecorationToContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Decoration'),
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
+
+      body: Container(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_wigets/adding_decoration_to_container.dart';
 
 main(){
   runApp(MyApp());  
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget{
       //   primarySwatch: Colors.blue    // you can also use ThemeData to set color according to you
       // ),
 
-      home: MainScreen(),
+      home: AddingDecorationToContainer(),
       
     );
     
