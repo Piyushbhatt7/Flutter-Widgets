83fa3125360177b9acb7480837434f4d12afe9f6
vengersmoon <vengersmon003@gmail.com>
2024-12-01 13:31:09 +0530

    Adding Decoration To Container

--- lib/adding_decoration_to_container.dart
+++ lib/adding_decoration_to_container.dart
@@ -17,6 +17,7 @@ class AddingDecorationToContainer extends StatelessWidget {
         height: double.infinity,
         child: Center(
           child: Container(
+           // child: Center(child: Text('Pussy!',style: TextStyle(fontSize: 30),)),
             height: 200,
             width: 200,      
           
@@ -26,7 +27,7 @@ class AddingDecorationToContainer extends StatelessWidget {
                
               // borderRadius: BorderRadius.only(topLeft: Radius.circular(21), bottomRight: Radius.circular(21)) // when using BorderRadius.only and BorderRadius.all you can't pass the value in double / you need to pass the value in Radius object like Radius.elliptical or circular and etc.
 
-               borderRadius: BorderRadius.all(Radius.circular(21)), // border radius.all work same as a border radius.only
+             //  borderRadius: BorderRadius.all(Radius.circular(21)), // border radius.all work same as a border radius.only
 
                border: Border.all(  // border.all provide a border to a whole container
                 width: 5,
@@ -38,7 +39,9 @@ class AddingDecorationToContainer extends StatelessWidget {
                   blurRadius: 52, // provide a shadow to a container
                  // spreadRadius: 8, // spread the radius 
                 )
-               ]
+               ],
+
+               shape: BoxShape.circle
             ),
           ),
         ),
