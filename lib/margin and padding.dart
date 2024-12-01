0cc84e242b9c43f51195ea057b57a6b64d4441d1
vengersmoon <vengersmon003@gmail.com>
2024-12-01 23:34:27 +0530

    Margin and Padding

--- lib/margin and padding.dart
+++ lib/margin and padding.dart
@@ -17,10 +17,11 @@ class margin_and_padding extends StatelessWidget {
       //   child: Text('Hello World !', style: TextStyle(fontSize: 21),),   // giving padding to all or can only
       // ),
 
-      body: Padding(
-        padding: const EdgeInsets.only(top: 12, left: 34),
-        child: Text('Hello World !', style: TextStyle(fontSize: 21),),    // giving margin 
-      )
+      body: Container(
+        color: Colors.orangeAccent,
+        margin: EdgeInsets.all(50),                      // margin provide spacing outside the container
+        child: Text('Hello World !',
+         style: TextStyle(fontSize: 21),))
     );
   }
 }
