074d551e0bd8ca270ee3bb342cf3899a6ea1c631
vengersmoon <vengersmon003@gmail.com>
2024-12-01 13:33:35 +0530

    Expanded widget

---
+++ lib/expanded widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Expanded_widget extends StatelessWidget {
+  const Expanded_widget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+       appBar: AppBar(
+        title: Text('Decoration'),
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
+
+      body: Container(),
+    );
+  }
+}
