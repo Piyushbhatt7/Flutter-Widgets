ff86cac4ce60d7dcccc35a0532fe692f696ee345
vengersmoon <vengersmon003@gmail.com>
2024-12-01 12:28:41 +0530

    Adding Decoration To Container

--- lib/adding_decoration_to_container.dart
+++ lib/adding_decoration_to_container.dart
@@ -23,7 +23,7 @@ class AddingDecorationToContainer extends StatelessWidget {
                color: Colors.amber,
               // borderRadius: BorderRadius.circular(12), // to make a circle use the half of container width [BorderRadius.circular is a shorcut way to change the edges of container]
                
-               borderRadius: BorderRadius.only()
+               borderRadius: BorderRadius.only(topLeft: ) // when using BorderRadius.only and BorderRadius.all you can't pass the value in double
             ),
           ),
         ),
