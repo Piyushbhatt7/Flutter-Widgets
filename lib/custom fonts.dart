579dd7149a9b4e62983a99ef813e582e272ae6ee
vengersmoon <vengersmon003@gmail.com>
2024-12-02 23:18:44 +0530

    Custom Fonts

---
+++ lib/custom fonts.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class custom_fonts extends StatelessWidget {
+  const custom_fonts({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Custom Fonts'),
+      ),
+    );
+  }
+}
