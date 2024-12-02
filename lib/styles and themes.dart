3a864bd7b6ce4a6a7ce7ab6b6096548e511f40f3
vengersmoon <vengersmon003@gmail.com>
2024-12-02 23:39:03 +0530

    Styles and Theme

---
+++ lib/styles and themes.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class styles_themes extends StatelessWidget {
+  const styles_themes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Styles and Theme'),
+      ),
+
+      body: ,
+    );
+  }
+}
