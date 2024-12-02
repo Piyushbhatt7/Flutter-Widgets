60044cb225cbd53a43e586f5490eb282f0ca2d18
vengersmoon <vengersmon003@gmail.com>
2024-12-02 11:37:07 +0530

    Circle Avatar Widget

--- lib/Circle Avatar.dart
+++ lib/Circle Avatar.dart
@@ -16,6 +16,7 @@ class Circle_Avatar extends StatelessWidget {
           backgroundImage: AssetImage('assets/images/lodu.png'),
           backgroundColor: Colors.transparent,
         //  radius: 100,                           while using radius no need to use container
+        minRadius: 10,
        ),
      ),
       
