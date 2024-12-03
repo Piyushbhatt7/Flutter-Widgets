957883c74e0c403e4c2bc2d7dedff0ce6dfbda53
vengersmoon <vengersmon003@gmail.com>
2024-12-03 12:53:58 +0530

    fontStyle: FontStyle.italic

--- lib/styles and themes.dart
+++ lib/styles and themes.dart
@@ -13,9 +13,9 @@ class styles_themes extends StatelessWidget {
       body: Column(
         children: [
           Text('Hello World', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-          Text('Hello World', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400, fontStyle: FontStyle.italic),),
+          Text('Hello World', style: TextStyle(fontSize: 10, fontWeight: FontWeight.w400, fontStyle: FontStyle.italic),),
           Text('Hello World', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-          Text('Hello World', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500,fontStyle: FontStyle.italic),),
+          Text('Hello World', style: TextStyle(fontSize: 10, fontWeight: FontWeight.w500,fontStyle: FontStyle.italic),),
         ],
       ),   
     );
