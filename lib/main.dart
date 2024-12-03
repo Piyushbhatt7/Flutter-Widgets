b2c1800e2b5bad6562947152e8a3a4b3533ffe92
vengersmoon <vengersmon003@gmail.com>
2024-12-03 14:27:51 +0530

    Styles and Theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_wigets/Circle%20Avatar.dart';
-import 'package:flutter_wigets/List%20Tile.dart';
-import 'package:flutter_wigets/ListviewBuilder.dart';
-import 'package:flutter_wigets/adding_decoration_to_container.dart';
-import 'package:flutter_wigets/custom%20fonts.dart';
-import 'package:flutter_wigets/expanded%20widget.dart';
-import 'package:flutter_wigets/margin%20and%20padding.dart';
-import 'package:flutter_wigets/styles%20and%20themes.dart';
 
 main(){
   runApp(MyApp());  
@@ -22,18 +14,17 @@ class MyApp extends StatelessWidget{
       theme: ThemeData(
         primarySwatch: Colors.blue,    // you can also use ThemeData to set color according to you
         textTheme: TextTheme(
-            headLine1: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
-            subtitle1: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            headlineLarge: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            headlineSmall: TextStyle(fontSize: 18, fontWeight: FontWeight.w300, fontStyle: FontStyle.italic),
         ),
       ),
       
 
-      home: styles_themes(),   
+      home: MainScreen(),   
       
     );
     
   }
-  
 }
 
 class MainScreen extends StatelessWidget{
@@ -44,16 +35,16 @@ class MainScreen extends StatelessWidget{
       appBar: AppBar(
         title: Text('Flutter Main Screen'),
 
-      ),
+      ),  
 
       body: Column(
         children: [
-          Text('Hello World', style: Theme.of(context).textTheme.headLine1),
-          Text('Hello World', style:),
-          Text('Hello World', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-          Text('Hello World', style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500,fontStyle: FontStyle.italic),),
+          Text('Hello World', style: Theme.of(context).textTheme.headlineLarge),
+          Text('Hello World', style: Theme.of(context).textTheme.headlineSmall,),
+          Text('Hello World', style: Theme.of(context).textTheme.headlineLarge,),
+          Text('Hello World', style:Theme.of(context).textTheme.headlineSmall,),
         ],
-      ),   
+      ),    
 
     );
   }
--- lib/styles and themes.dart
+++ lib/styles and themes.dart
@@ -1,23 +1,23 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class styles_themes extends StatelessWidget {
-  const styles_themes({super.key});
+// class styles_themes extends StatelessWidget {
+//   const styles_themes({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Styles and Theme'),
-      ),
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Styles and Theme'),
+//       ),
 
-      body: Column(
-        children: [
-          Text('Hello World', style: Theme.of(context).textTheme.headLine1),
-          Text('Hello World', style:),
-          Text('Hello World', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-          Text('Hello World', style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500,fontStyle: FontStyle.italic),),
-        ],
-      ),   
-    );
-  }
-}
+//       body: Column(
+//         children: [
+//           Text('Hello World', style: Theme.of(context).textTheme.headLine1),
+//           Text('Hello World', style:),
+//           Text('Hello World', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+//           Text('Hello World', style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500,fontStyle: FontStyle.italic),),
+//         ],
+//       ),   
+//     );
+//   }
+// }
