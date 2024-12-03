50514493372accd734a32cced4448e22f3c6aa28
vengersmoon <vengersmon003@gmail.com>
2024-12-03 22:45:45 +0530

    style and theme

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class MainScreen extends StatelessWidget{
         child: Column(
         // mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Hello', style: Theme.of(context).textTheme.headlineLarge),
+           // Text('Hello', style: Theme.of(context).textTheme.headlineLarge.co),
             Text('World', style: Theme.of(context).textTheme.headlineSmall,),
             Text('Hello', style: Theme.of(context).textTheme.headlineLarge,),
             Text('World', style:Theme.of(context).textTheme.headlineSmall,),
