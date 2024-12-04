54c217d13c2fdc36d63887cbe0fc264c61d4d952
vengersmoon <vengersmon003@gmail.com>
2024-12-04 22:53:13 +0530

    Cuttent Date & Time

--- lib/current Date & Time.dart
+++ lib/current Date & Time.dart
@@ -5,9 +5,11 @@ class Current_date_time extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var time = DateTime.now();
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Cuttent Date & Time'),
+        title: Text('Cuttent Time ${time}'),
         backgroundColor: Colors.lightBlueAccent,
       ),
     );
