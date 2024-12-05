ad002dacf2b2914460b926030da4a14c1c1be252
vengersmoon <vengersmon003@gmail.com>
2024-12-05 23:00:25 +0530

    GridView

--- lib/GridView.dart
+++ lib/GridView.dart
@@ -40,6 +40,24 @@ class Gridview extends StatelessWidget {
       // ],    
       
       // ),
+
+
+      body: GridView.count(crossAxisCount: 3, // devide the row
+      crossAxisSpacing: 11,  
+      mainAxisSpacing: 11,
+      children: [
+         Container(color: arrColors[0],),
+         Container(color: arrColors[1],),
+         Container(color: arrColors[2],),
+         Container(color: arrColors[3],),
+         Container(color: arrColors[4],),
+         Container(color: arrColors[5],),
+         Container(color: arrColors[6],),
+         Container(color: arrColors[7],),
+         Container(color: arrColors[8],),
+      ],    
+      
+      ),
       
     );
   }
