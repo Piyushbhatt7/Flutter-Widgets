9d0ae986a77070eac163585be4dad400fe8c44fe
vengersmoon <vengersmon003@gmail.com>
2024-12-05 11:29:59 +0530

    Current date and time

---
+++ lib/DateFormat Patterns.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Dateformat_patterns extends StatefulWidget {
+  const Dateformat_patterns({super.key});
+
+  @override
+  State<Dateformat_patterns> createState() => _Dateformat_patternsState();
+}
+
+class _Dateformat_patternsState extends State<Dateformat_patterns> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: ,
+      ),
+    );
+  }
+}
