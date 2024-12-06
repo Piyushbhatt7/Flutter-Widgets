66f666b406804d2fdb08b0856d33bae5735e0734
vengersmoon <vengersmon003@gmail.com>
2024-12-06 10:06:59 +0530

    main dart file

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'package:flutter_wigets/Card%20Widget.dart';
 import 'package:flutter_wigets/DateFormat%20Patterns.dart';
 import 'package:flutter_wigets/Gridview.dart';
 import 'package:flutter_wigets/Gridviewrowcolumn.dart';
+import 'package:flutter_wigets/How%20to%20add%20image.dart';
 import 'package:flutter_wigets/Text%20Field.dart';
 import 'package:flutter_wigets/Themes/styles%20and%20themes.dart';
+import 'package:flutter_wigets/adding_decoration_to_container.dart';
 import 'package:flutter_wigets/current%20Date%20&%20Time.dart';
 import 'package:flutter_wigets/datepicker.dart';
 
@@ -29,7 +31,7 @@ class MyApp extends StatelessWidget{
       ),
       
 
-      home: Gridview(),   
+      home: AddingDecorationToContainer(),   
       
     );
     
