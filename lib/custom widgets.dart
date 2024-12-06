0fda30c736421c892c5f8a643227a5837a92d842
vengersmoon <vengersmon003@gmail.com>
2024-12-06 22:56:21 +0530

    Custom widgets

--- lib/custom widgets.dart
+++ lib/custom widgets.dart
@@ -39,10 +39,13 @@ class custom_widget extends StatelessWidget {
                ),
        ),
       
-       Container(
-        height: 200,
-        color: Colors.redAccent,
-      ),
+       Expanded(
+        flex: 2,
+         child: Container(
+          height: 200,
+          color: Colors.redAccent,
+               ),
+       ),
          
          
           ],
