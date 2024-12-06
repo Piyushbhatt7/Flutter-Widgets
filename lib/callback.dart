02ba5ea2be285c012833133762aa36e60efac814
vengersmoon <vengersmon003@gmail.com>
2024-12-06 14:45:37 +0530

    Callback

---
+++ lib/callback.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Callback extends StatelessWidget {
+  const Callback({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('CallBack'),
+      ),
+
+      body: ElevatedButton(onPressed: (){
+        
+      }, child: child)
+    );
+  }
+}
