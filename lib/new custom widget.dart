ce432109e5a200da468d21efaa22393ebe22db5d
vengersmoon <vengersmon003@gmail.com>
2024-12-14 14:05:42 +0530

    d

--- lib/new custom widget.dart
+++ lib/new custom widget.dart
@@ -10,6 +10,10 @@ class new_custom extends StatefulWidget {
 class _new_customState extends State<new_custom> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        
+      ),
+    );
   }
 }
