57dfa980a658369c6b35b9ca135ba1d8b1f0a70f
vengersmoon <vengersmon003@gmail.com>
2024-12-14 22:39:39 +0530

    d

--- lib/stack.dart
+++ lib/stack.dart
@@ -1,8 +1,16 @@
+import 'package:flutter/material.dart';
+
 class Stack extends StatelessWidget {
   const Stack({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text('Stack widget'),
+        
+      ),
+    );
   }
 }
