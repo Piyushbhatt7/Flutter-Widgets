1361f788269daeca725de195f3e83842d374face
vengersmoon <vengersmon003@gmail.com>
2024-12-14 21:14:22 +0530

    d

--- lib/new custom widget.dart
+++ lib/new custom widget.dart
@@ -1,8 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class RoundedButtom extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+
+    return ElevatedButton(onPressed: onPressed, child: child)
     
   }
     
