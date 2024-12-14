aee6ee41c4b43affdee19f1c1bf281f82f3958d2
vengersmoon <vengersmon003@gmail.com>
2024-12-14 21:16:30 +0530

    d

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget{
       ),
       
 
-      home: new_custom(),   
+      home: RoundedButtom(),   
       
     );
     
--- lib/new custom widget.dart
+++ lib/new custom widget.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class RoundedButtom extends StatelessWidget{
+
+ 
+
+
+
   @override
   Widget build(BuildContext context) {
 
