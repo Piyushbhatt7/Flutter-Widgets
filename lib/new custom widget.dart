1cfbaf328632b13e21e26db43563277edbfd3c8a
vengersmoon <vengersmon003@gmail.com>
2024-12-14 14:06:02 +0530

    d

--- lib/new custom widget.dart
+++ lib/new custom widget.dart
@@ -12,7 +12,7 @@ class _new_customState extends State<new_custom> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        
+        backgroundColor: Colors.yellowAccent,
       ),
     );
   }
