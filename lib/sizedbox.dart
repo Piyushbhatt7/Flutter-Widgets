d4964a3565fb70e06b0e97eea5421683f396a426
vengersmoon <vengersmon003@gmail.com>
2024-12-15 14:20:39 +0530

    s

--- lib/sizedbox.dart
+++ lib/sizedbox.dart
@@ -5,6 +5,11 @@ class sized_box extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text('Size box'),
+      ),
+    );
   }
 }
