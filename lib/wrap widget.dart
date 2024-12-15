13f1fa31971bfc4f1dcd29a80902be6c8530848a
vengersmoon <vengersmon003@gmail.com>
2024-12-15 13:37:09 +0530

    f

--- lib/wrap widget.dart
+++ lib/wrap widget.dart
@@ -47,7 +47,7 @@ class Wrap_widget extends StatelessWidget {
 
             Container(
               height: 100,
-              width: 100,
+              width: 300,
               color: Colors.purpleAccent,
             ),
 
