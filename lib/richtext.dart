7402ffff64bded5003cff24967881b6d4d6eb164
vengersmoon <vengersmon003@gmail.com>
2024-12-15 14:39:52 +0530

    d

---
+++ lib/richtext.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Rich_text extends StatelessWidget {
+  const Rich_text({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
