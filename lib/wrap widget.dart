8816464694a66b04df5b2caf475e5fcd439a3c7b
vengersmoon <vengersmon003@gmail.com>
2024-12-15 13:32:12 +0530

    ff

--- lib/wrap widget.dart
+++ lib/wrap widget.dart
@@ -5,6 +5,11 @@ class Wrap_widget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: Text('Wrap wiget'),
+      ),
+    );
   }
 }
