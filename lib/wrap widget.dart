d524bdaf7573e588b353f66877591f7c672ea67f
vengersmoon <vengersmon003@gmail.com>
2024-12-15 14:06:32 +0530

    d

--- lib/wrap widget.dart
+++ lib/wrap widget.dart
@@ -11,75 +11,78 @@ class Wrap_widget extends StatelessWidget {
         title: Text('Wrap wiget'),
       ),
 
-        body: Wrap( 
-          direction: Axis.horizontal,    // the  wrap widget work for both row and column
-          spacing: 11,
-          runSpacing: 11,
-          alignment: WrapAlignment.center,
-          children: [
-            Container(
-              height: 100,
-              width: 300,
-              color: Colors.amberAccent,
-            ),
-
-            Container(
-              height: 100,
-              width: 300,
-              color: Colors.green,
-            ),
-
-
-            Container(
-              height: 100,
-              width: 300,
-              color: Colors.deepPurple,
-            ),
-
-            Container(
-              height: 100,
-              width: 300,
-              color: Colors.blueAccent,
-            ),
-
-            Container(
-              height: 100,
-              width: 300,
-              color: Colors.pink,
-            ),
-
-
-            Container(
-              height: 100,
-              width: 300,
-              color: Colors.purpleAccent,
-            ),
-
-
-            Container(
-              height: 100,
-              width: 300,
-              color: Colors.lime,
-            ),
-
-            Container(
-              height: 100,
-              width: 300,
-              color: Colors.black87,
-            ),
-
-             Container(
-              height: 100,
-              width: 300,
-              color: Colors.deepOrange,
-            ),
-
-             Container(
-              height: 100,
-              width: 300,
-              color: Colors.grey,
-            ),
-          ],
+        body: Container(
+          width: double.infinity,
+          child: Wrap( 
+            direction: Axis.horizontal,    // the  wrap widget work for both row and column
+            spacing: 11,
+            runSpacing: 11,
+            alignment: WrapAlignment.center,
+            children: [
+              Container(
+                height: 100,
+                width: 300,
+                color: Colors.amberAccent,
+              ),
+          
+              Container(
+                height: 100,
+                width: 300,
+                color: Colors.green,
+              ),
+          
+          
+              Container(
+                height: 100,
+                width: 300,
+                color: Colors.deepPurple,
+              ),
+          
+              Container(
+                height: 100,
+                width: 300,
+                color: Colors.blueAccent,
+              ),
+          
+              Container(
+                height: 100,
+                width: 300,
+                color: Colors.pink,
+              ),
+          
+          
+              Container(
+                height: 100,
+                width: 300,
+                color: Colors.purpleAccent,
+              ),
+          
+          
+              Container(
+                height: 100,
+                width: 300,
+                color: Colors.lime,
+              ),
+          
+              Container(
+                height: 100,
+                width: 300,
+                color: Colors.black87,
+              ),
+          
+               Container(
+                height: 100,
+                width: 300,
+                color: Colors.deepOrange,
+              ),
+          
+               Container(
+                height: 100,
+                width: 300,
+                color: Colors.grey,
+              ),
+            ],
+          ),
         ),
     );
   }
