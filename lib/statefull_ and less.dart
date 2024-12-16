32716898b0e2747f6f0b0e119e210f83fc2391c4
vengersmoon <vengersmon003@gmail.com>
2024-12-16 14:28:14 +0530

    a

---
+++ lib/statefull_ and less.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class full_widget extends StatefulWidget {
+  const full_widget({super.key});
+
+  @override
+  State<full_widget> createState() => _full_widgetState();
+}
+
+class _full_widgetState extends State<full_widget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
