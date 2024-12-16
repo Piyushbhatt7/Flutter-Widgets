ea824efb825ea048dc7ae4fb8786c1a20ba3a4e8
vengersmoon <vengersmon003@gmail.com>
2024-12-16 11:53:48 +0530

    d

---
+++ lib/awesome icon.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class awesome_icon extends StatelessWidget {
+  const awesome_icon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.tealAccent,
+        title: Text('Icon widget'),
+        
+    ),
+
+    body: Center(child: Icon(Icons.play_circle_fill, size: 100, color: Colors.purple,)),    // giving a tint to an Icon
+
+    );
+  } 
+}   
