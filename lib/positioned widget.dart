6751a19f56d95ebfbe1c60890f3dbf76fcc88cdc
vengersmoon <vengersmon003@gmail.com>
2024-12-16 12:04:15 +0530

    f

---
+++ lib/positioned widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Positioned_widget extends StatelessWidget {
+  const Positioned_widget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       
+       appBar: AppBar(
+         backgroundColor: Colors.tealAccent,
+         title: ,
+       ),
+    );
+  }
+}
