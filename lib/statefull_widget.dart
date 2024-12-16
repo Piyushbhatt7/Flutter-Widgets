7bf2fb1b7bd143dc17908507992be6c0daec57e8
vengersmoon <vengersmon003@gmail.com>
2024-12-16 22:39:19 +0530

    d

--- lib/statefull_widget.dart
+++ lib/statefull_widget.dart
@@ -20,7 +20,7 @@ class _statefull_widgetState extends State<statefull_widget> {
       ),
 
       body: Container(
-        color: Colors.lightBlue,
+        color: Colors.blue.shade50,
         child: Center(
           child: Padding(
             padding: const EdgeInsets.all(8.0),
