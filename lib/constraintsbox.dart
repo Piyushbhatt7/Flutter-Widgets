05b2541ba7dfce057e9fcf0adc21e82d53dcb98b
vengersmoon <vengersmon003@gmail.com>
2024-12-17 15:20:29 +0530

    s

--- lib/constraintsbox.dart
+++ lib/constraintsbox.dart
@@ -5,6 +5,10 @@ class constraint_box extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Constraint Box'),
+      ),
+    );
   }
 }
