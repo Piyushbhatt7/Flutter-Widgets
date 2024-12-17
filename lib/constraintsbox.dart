9b0a5686a02a78c10c81895450bad3a5ad4aa9be
vengersmoon <vengersmon003@gmail.com>
2024-12-17 15:19:51 +0530

    s

---
+++ lib/constraintsbox.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class constraint_box extends StatelessWidget {
+  const constraint_box({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
