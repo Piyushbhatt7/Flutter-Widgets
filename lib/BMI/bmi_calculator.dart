7f1b6b9bf6e802ec37f4f1e1e3b0f3e83cf4c6c1
vengersmoon <vengersmon003@gmail.com>
2024-12-17 21:31:52 +0530

    a

---
+++ lib/BMI/bmi_calculator.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BmiCalculator extends StatefulWidget {
+  const BmiCalculator({super.key});
+
+  @override
+  State<BmiCalculator> createState() => _BmiCalculatorState();
+}
+
+class _BmiCalculatorState extends State<BmiCalculator> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
