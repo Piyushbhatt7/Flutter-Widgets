e219fea111c322cc21cad33207e168937a83a85c
vengersmoon <vengersmon003@gmail.com>
2024-12-18 13:37:59 +0530

    Flutter different widgets

--- lib/IntroPage.dart
+++ lib/IntroPage.dart
@@ -2,32 +2,34 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_wigets/main.dart';
 
-class Intropage  extends StatelessWidget{
+class Intropage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    return  Scaffold(
-             
-        appBar: AppBar(
-          title: Text('Intro Page'),
-        ),
-
-        body: Column(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Intro Page'),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-             Text('Welcome', style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),),
-
-             SizedBox(height: 12,),
-
-             ElevatedButton(onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context){
-                        
-                        return MyApp();
+            Text(
+              'Welcome',
+              style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return MyApp();
                   }));
-             }, child: Text('Next'))
+                },
+                child: Text('Next'))
           ],
         ),
+      ),
     );
-    
   }
-
 }
