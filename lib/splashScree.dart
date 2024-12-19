fb0a976e566894ce3417054cf51a430e965efaab
vengersmoon <vengersmon003@gmail.com>
2024-12-19 13:17:47 +0530

    d

--- lib/splashScree.dart
+++ lib/splashScree.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Splashscreen extends StatelessWidget {
+class Splashscreen extends StatefulWidget {
+  
   const Splashscreen({super.key});
 
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
