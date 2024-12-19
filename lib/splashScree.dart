a4830c795caec08d914beaa4ca4ed400d1cadb51
vengersmoon <vengersmon003@gmail.com>
2024-12-19 12:56:00 +0530

    e

--- lib/splashScree.dart
+++ lib/splashScree.dart
@@ -15,7 +15,7 @@ class Splashscreen extends StatelessWidget {
       body: Container(
          color: Colors.indigo,
          child: Center(
-          child: Text(data),
+          child: Text('Classico'),
          ),
       ),
     );
