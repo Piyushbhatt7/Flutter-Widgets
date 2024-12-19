d4cd79c0ef3e567f03542584fb470956a9560b83
vengersmoon <vengersmon003@gmail.com>
2024-12-19 13:39:25 +0530

    s

---
+++ lib/dashScreen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class DashScreen extends StatefulWidget {
+  @override
+  _DashScreenState createState() => _DashScreenState();
+}
+
+class _DashScreenState extends State<DashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dash Screen'),
+      ),
+      body: Center(
+        child: Text('Welcome to Dash Screen!'),
+      ),
+    );
+  }
+}
