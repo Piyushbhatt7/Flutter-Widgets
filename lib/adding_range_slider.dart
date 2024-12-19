44c94332af72388523cb9110d548fcf039d753c8
vengersmoon <vengersmon003@gmail.com>
2024-12-19 14:02:56 +0530

    d

--- lib/adding_range_slider.dart
+++ lib/adding_range_slider.dart
@@ -3,12 +3,22 @@ import 'package:flutter/material.dart';
 class AddingRangeSlider extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    RangeValues values = RangeValues(0, 5);
+
+    RangeLabels labels = RangeLabels(values.start.toString(), values.end.toString());
     return Scaffold(
       appBar: AppBar(
         title: Text('Range Slider Example'),
         backgroundColor: Colors.blueGrey,
       ),
-      body: 
+      body: Center(
+        child: RangeSlider(
+          values: values, 
+          onChanged: (newValue){
+
+          }),
+      )
     );
   }
 }
