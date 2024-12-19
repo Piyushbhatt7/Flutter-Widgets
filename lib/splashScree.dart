cb81a97f4cdddca39d7d0af526408698bfdb27d4
vengersmoon <vengersmon003@gmail.com>
2024-12-19 11:47:46 +0530

    s

---
+++ lib/splashScree.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class Splashscreen extends StatelessWidget {
+  const Splashscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
