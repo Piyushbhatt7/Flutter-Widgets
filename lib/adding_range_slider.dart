0acdb3a5bcd23a57d9e9404fc02b4ecdad21248a
vengersmoon <vengersmon003@gmail.com>
2024-12-19 22:32:41 +0530

    d

--- lib/adding_range_slider.dart
+++ lib/adding_range_slider.dart
@@ -2,16 +2,16 @@ import 'package:flutter/material.dart';
 
 class AddingRangeSlider extends StatefulWidget {
   @override
-  State<AddingRangeSlider> createState() => _AddingRangeSliderState();
+  _AddingRangeSliderState createState() => _AddingRangeSliderState();
 }
 
 class _AddingRangeSliderState extends State<AddingRangeSlider> {
+  RangeValues _values = RangeValues(0, 1);
+
   @override
   Widget build(BuildContext context) {
+    RangeLabels labels = RangeLabels(_values.start.toString(), _values.end.toString());
 
-    RangeValues values = RangeValues(0, 1);
-
-    RangeLabels labels = RangeLabels(values.start.toString(), values.end.toString());
     return Scaffold(
       appBar: AppBar(
         title: Text('Range Slider Example'),
@@ -19,20 +19,19 @@ class _AddingRangeSliderState extends State<AddingRangeSlider> {
       ),
       body: Center(
         child: RangeSlider(
-          values: values, 
+          values: _values,
           labels: labels,
           divisions: 10,
           max: 100,
           min: 0,
-          onChanged: (newValue){
-              setState(() {
-                  values = newValue;
-              });
-
-                            
-              print('${newValue.start}, ${newValue.end}');
-          }),
-      )
+          onChanged: (newValue) {
+            setState(() {
+              _values = newValue;
+            });
+            print('${newValue.start}, ${newValue.end}');
+          },
+        ),
+      ),
     );
   }
 }
