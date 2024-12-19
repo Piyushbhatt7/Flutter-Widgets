c28be52279a5c816c8ec3a91e660ebf87fd09fc6
vengersmoon <vengersmon003@gmail.com>
2024-12-19 11:49:15 +0530

    a

--- lib/splashScree.dart
+++ lib/splashScree.dart
@@ -10,7 +10,7 @@ class Splashscreen extends StatelessWidget {
         
         appBar: AppBar(
           backgroundColor: Colors.indigoAccent,
-          title: Text('Splash screen'),
+          title: Center(child: Text('Splash screen')),
         ),
     );
   }
