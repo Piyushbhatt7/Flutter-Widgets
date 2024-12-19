4ad3e090f872a33736fb42ada7e01165f9c7c1a7
vengersmoon <vengersmon003@gmail.com>
2024-12-19 11:48:03 +0530

    a

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:flutter_wigets/maps.dart';
 import 'package:flutter_wigets/positioned%20widget.dart';
 import 'package:flutter_wigets/richtext.dart';
 import 'package:flutter_wigets/sizedbox.dart';
+import 'package:flutter_wigets/splashScree.dart';
 import 'package:flutter_wigets/statefull_%20and%20less.dart';
 import 'package:flutter_wigets/statefull_widget.dart';
 import 'package:flutter_wigets/wrap%20widget.dart';
@@ -35,7 +36,7 @@ class MyApp extends StatelessWidget {
         ),
       ),
 
-      home: Intropage()
+      home: Splashscreen()
     );
   }
 }
