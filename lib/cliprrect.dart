c87a61b3d3d56e969516d328f6c19adfff4380b8
vengersmoon <vengersmon003@gmail.com>
2024-12-20 11:20:42 +0530

    a

---
+++ lib/cliprrect.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Cliprrect extends StatelessWidget {
+  const Cliprrect({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_wigets/adding_range_slider.dart';
+import 'package:flutter_wigets/cliprrect.dart';
 import 'package:flutter_wigets/gradient.dart';
 
 
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
         ),
       ),
 
-      home: Gradient_color()
+      home: Cliprrect(),
     );
   }
 }
