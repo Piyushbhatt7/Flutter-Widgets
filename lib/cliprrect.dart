9374194e944ba8e241ac8f73db632b3105e4770c
vengersmoon <vengersmon003@gmail.com>
2024-12-20 11:21:04 +0530

    a

--- lib/cliprrect.dart
+++ lib/cliprrect.dart
@@ -5,6 +5,10 @@ class Cliprrect extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.cyanAccent.shade200,
+      ),
+    );
   }
 }
