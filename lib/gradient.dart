03a91ffc039eddb90c9c5b46d30823a679b2d392
vengersmoon <vengersmon003@gmail.com>
2024-12-20 11:16:47 +0530

    A

--- lib/gradient.dart
+++ lib/gradient.dart
@@ -16,14 +16,14 @@ class Gradient_color extends StatelessWidget {
           height: 480,
           width: 400,
           decoration: BoxDecoration(
-            gradient: LinearGradient(
+            gradient: RadialGradient(
               colors: [
                  Color(0XFFFFFECD2),
                  Color(0xfffCB69F),
                 
               ],
         
-             // center: Alignment.center,
+              center: Alignment.center,
               stops: [0.0, 1.0],
             
             )
