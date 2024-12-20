d51f6a1a303028c2130002a20d3287c6cd09eb1e
vengersmoon <vengersmon003@gmail.com>
2024-12-21 00:43:13 +0530

    d

--- lib/foo animation.dart
+++ lib/foo animation.dart
@@ -34,7 +34,7 @@ class _Foo_animationState extends State<Foo_animation> {
               ),
               ElevatedButton(
                   onPressed: () {
-                    setState(() {
+                    setState(() { 
                       if (flag) {
                         _width = 150.0;
                         _height = 260.0;
