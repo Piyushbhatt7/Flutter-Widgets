c5c31517648c2e6e79986e4caaf39fc9f966bc8c
vengersmoon <vengersmon003@gmail.com>
2024-12-20 10:36:56 +0530

    a

--- lib/gradient.dart
+++ lib/gradient.dart
@@ -1,8 +1,15 @@
+import 'package:flutter/material.dart';
+
 class Gradient_color extends StatelessWidget {
   const Gradient_color({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Gradient color'),
+        backgroundColor: Colors.lightBlueAccent.shade400,
+      ),
+    );
   }
 }
