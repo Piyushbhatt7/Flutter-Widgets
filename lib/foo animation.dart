92d2644dedfb495ab3ba790542109ff66a617fe0
vengersmoon <vengersmon003@gmail.com>
2024-12-21 12:08:08 +0530

    a

--- lib/foo animation.dart
+++ lib/foo animation.dart
@@ -12,6 +12,9 @@ class _Foo_animationState extends State<Foo_animation> {
   double _height = 150.0;
   bool flag = false;
   Color bgcolr = Colors.blueGrey;
+  Decoration mydeco = BoxDecoration(
+    borderRadius: BorderRadius.circular(10)
+  );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,6 +31,7 @@ class _Foo_animationState extends State<Foo_animation> {
                 width: _width,
                 height: _height,
                 duration: Duration(seconds: 3),
+                decoration: mydeco,
               ),
               SizedBox(
                 height: 10,
