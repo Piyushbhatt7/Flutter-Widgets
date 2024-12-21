3bd0695ccfaf0e7fa06e4dee3ea267c0bcc6540f
vengersmoon <vengersmon003@gmail.com>
2024-12-21 12:51:49 +0530

    f

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_wigets/animatedopacity.dart';
 import 'package:flutter_wigets/foo%20animation.dart';
 
 
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
         ),
       ),
 
-      home: Foo_animation(),
+      home: Animated_opacity(),
     );
   }
 }
