0bbdc84a6ea2d508e5bc62f41866868acd756425
vengersmoon <vengersmon003@gmail.com>
2024-12-21 14:55:50 +0530

    fds

--- lib/cossfade.dart
+++ lib/cossfade.dart
@@ -11,16 +11,14 @@ class _crossfadeState extends State<crossfade> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.amber.shade400,
-        title: Text('Cross Fade'),
-      ),
-
-      body: Column(
-        children: [
-          
-        ],
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: Colors.amber.shade400,
+          title: Text('Cross Fade'),
+        ),
+        body: AnimatedCrossFade(
+            firstChild: firstChild,
+            secondChild: secondChild,
+            crossFadeState: crossFadeState,
+            duration: duration));
   }
 }
