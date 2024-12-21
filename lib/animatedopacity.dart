ad571ec17f23e06ae2a99fe0c51fbe11ed32eebd
vengersmoon <vengersmon003@gmail.com>
2024-12-21 12:56:57 +0530

    gg

--- lib/animatedopacity.dart
+++ lib/animatedopacity.dart
@@ -5,6 +5,11 @@ class Animated_opacity extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amberAccent,
+        title: Text('Animated container'),
+      ),
+    );
   }
 }
