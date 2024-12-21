f0d2504b7bca3c090d6911f821054d226b237ca4
vengersmoon <vengersmon003@gmail.com>
2024-12-21 15:23:08 +0530

    flutter different widgets

--- lib/cossfade.dart
+++ lib/cossfade.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
 class crossfade extends StatefulWidget {
@@ -10,46 +8,67 @@ class crossfade extends StatefulWidget {
 }
 
 class _crossfadeState extends State<crossfade> {
- 
- bool isfirst = true;
+  bool isfirst = true;
+
   @override
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 4), (){
-         
-         reload();
-        
+    // Timer(Duration(seconds: 2), (){
+
+    //      reload();
+
+    // });
+  }
+
+  void reload() {
+    setState(() {
+      if (isfirst) {
+        isfirst = false;
+      } else {
+        isfirst = true;
+      }
     });
   }
 
-    void reload(){
-      setState(() {
-          isfirst = false;
-      });
-    }
-  
-
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.amber.shade400,
-          title: Text('Cross Fade'),
-        ),
-        body: Center(
-          child: AnimatedCrossFade(
+      appBar: AppBar(
+        backgroundColor: Colors.amber.shade400,
+        title: Text('Cross Fade'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedCrossFade(
               firstChild: Container(
                 width: 200,
                 height: 200,
                 color: Colors.grey,
               ),
-              secondChild: Image.asset("assets/images/bob.png",width: 200, height: 200,),
-              crossFadeState: isfirst? CrossFadeState.showFirst : CrossFadeState.showSecond ,
-              duration: Duration(seconds: 2)
-              
-          ),
+              secondChild: Image.asset(
+                "assets/images/bob.png",
+                width: 200,
+                height: 200,
+              ),
+              crossFadeState: isfirst
+                  ? CrossFadeState.showFirst
+                  : CrossFadeState.showSecond,
+              duration: Duration(seconds: 2),
+              sizeCurve: Curves.bounceIn,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  isfirst = true;
+                  setState(() {
+                    reload();
+                  });
+                },
+                child: Text('Click'))
+          ],
         ),
-            );
+      ),
+    );
   }
 }
