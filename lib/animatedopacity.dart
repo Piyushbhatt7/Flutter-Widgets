966999d0d4c9b6c09f6615d75ea5bee54bf6b919
vengersmoon <vengersmon003@gmail.com>
2024-12-21 12:51:32 +0530

    f

---
+++ lib/animatedopacity.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Animated_opacity extends StatelessWidget {
+  const Animated_opacity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
